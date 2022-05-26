d712849ce43f46b2b32eef7237dc8343253b5fa7
Sarah West <west0883@umn.edu>
2022-05-26 09:47:39 -0500

    Fix to loop errors.

--- SVD_forMSI_function.m
+++ SVD_forMSI_function.m
@@ -16,7 +16,7 @@ function []=SVD_forMSI_function(mouse_number)
     n_compressions=200;
     
     folder=pwd;
-    addpath(genpath(folder));
+    %addpath(genpath(folder));
     dir_in=[folder '/fully preprocessed stacks/' mouse '/']; % directory on the MSI network. 
     dir_out=[folder '/' ]; % directory on the MSI network.
     
@@ -34,7 +34,7 @@ function []=SVD_forMSI_function(mouse_number)
 
     % Start a running count of how many stacks each mouse has; for data space pre-alotment
     total_stacks=0;
-    
+    stacks = [];
     % For each  day; count how many stacks are in each day and add them all up so you can make an accurately sized matrix for data pre-alotment
     for dayi=1:size(mice_all(mousei).days,2)  
         
@@ -43,25 +43,24 @@ function []=SVD_forMSI_function(mouse_number)
         
         all_stacks = [mice_all(mousei).days(dayi).stacks mice_all(mousei).days(dayi).spontaneous];
         all_stacks = all_stacks(~isnan(all_stacks));
-        stacks = [];
+        
         % Get list of stacks for that day from mice_all
-        for stacki = 1: size(all_stacks)
-            
-            if ~isempty(all_stacks)
+        for stacki = 1: numel(all_stacks)
             % List the stacks in a given day
-            stacks= [stacks; sprintf('data%02d.mat', all_stacks(stacki))];  
-            end
+            filename = sprintf('data%02d.mat', all_stacks(stacki));
+            stacks= [stacks; [dir_in day '/' filename]]; 
         end
 
         % Add the number of stacks to the running count
         total_stacks=total_stacks+size(all_stacks,2); 
     end 
+
     disp(['total stacks =' num2str(total_stacks)]); 
     
     % Get infor from first stack to get nummber of pixels
     day=mice_all(mousei).days(1).name; 
      
-    matObj = matfile([dir_in day '/' stacks(1,:)]);
+    matObj = matfile([stacks(1,:)]);
     pixels = size(matObj,'data', 1);
     frames=size(matObj, 'data' ,2);
    
@@ -70,22 +69,13 @@ function []=SVD_forMSI_function(mouse_number)
 
     % load and concatenate data
     disp('concatenating');
-    count=0;
-    
-    % For each  day; 
-    for dayi=1:size(mice_all(mousei).days,2)  
-    
-        % Get the day name.
-        day=mice_all(mousei).days(dayi).name; 
-        
-        % List the  stacks in a given day
-        stacks=dir([dir_in day '/data*.mat']);  
-        for stacki=1:size(stacks,1) % for each stack
-            load([dir_in day '/' stacks(stacki).name]);  
-            all_data((count*frames)+1:(count+1)*frames, :)=data';
-            count=count+1;
-        end 
-    end
+    count=0; 
+    % for each stack
+    for stacki=1:size(stacks,1) 
+        load(stacks(stacki,:));  
+        all_data((count*frames)+1:(count+1)*frames, :)=data';
+        count=count+1;
+    end 
 
     % run SVD   
     disp('running SVD'); 
