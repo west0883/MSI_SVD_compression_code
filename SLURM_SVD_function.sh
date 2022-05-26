5db42ad92fe7dad548657fc55c29b0e926cde097
Sarah West <west0883@umn.edu>
2022-05-26 09:47:42 -0500

    Adjust time and memory limits.

--- SLURM_SVD_function.sh
+++ SLURM_SVD_function.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l        
-#SBATCH --time=5:00:00
+#SBATCH --time=7:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=200g
+#SBATCH --mem=350g
 #SBATCH --mail-type=ALL  
 #SBATCH --mail-user=west0883@umn.edu 
 #SBATCH -p ram1t
