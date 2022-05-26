d99e696dd4e30813fc9f36cfee1aeb156ba44ae7
Sarah West <west0883@umn.edu>
2022-05-26 09:47:33 -0500

    Change to array of mice.

--- SLURM_SVD_function.sh
+++ SLURM_SVD_function.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l        
-#SBATCH --time=3:00:00
+#SBATCH --time=5:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=350g
+#SBATCH --mem=200g
 #SBATCH --mail-type=ALL  
 #SBATCH --mail-user=west0883@umn.edu 
 #SBATCH -p ram1t
-#SBATCH --array=1087
+#SBATCH --array=1087, 1088, 1096
 
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
