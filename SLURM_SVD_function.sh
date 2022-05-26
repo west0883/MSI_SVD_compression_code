5c7c1daf3cd62795cda276545fcb1a2c9079cfe3
Sarah West <west0883@umn.edu>
2022-05-26 09:47:48 -0500

    Increase time for 500 components.

--- SLURM_SVD_function.sh
+++ SLURM_SVD_function.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l        
-#SBATCH --time=4:00:00
+#SBATCH --time=6:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=225g
 #SBATCH --mail-type=ALL  
 #SBATCH --mail-user=west0883@umn.edu 
 #SBATCH -p ram1t
-#SBATCH --array=1087,1088,1096
+#SBATCH --array=1100
 
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
